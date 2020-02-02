0cd22f1fd94f4c961bbc58c229e78b08c61f6ba7
dostontiu <dostonberuniy@gmail.com>
2020-02-03 00:01:46 +0500

    add.php 03.02

---
+++ backend/views/pilgrim/_right.php
@@ -0,0 +1,38 @@
+<table class="table table-hover table-bordered">
+    <thead>
+    <tr>
+        <th>#</th>
+        <th>Familiya</th>
+        <th>Ism</th>
+        <th>Pasport raqam</th>
+        <th>Tugilgan kun</th>
+        <th></th>
+    </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td>surname</td>
+            <td>name</td>
+            <td>p_number</td>
+            <td>12.12.1966</td>
+            <td>$img</td>
+        </tr>
+        <tr>
+            <th scope="row">1</th>
+            <td>surname</td>
+            <td>name</td>
+            <td>p_number</td>
+            <td>12.12.1966</td>
+            <td>$img</td>
+        </tr>
+        <tr>
+            <th scope="row">1</th>
+            <td>surname</td>
+            <td>name</td>
+            <td>p_number</td>
+            <td>12.12.1966</td>
+            <td>$img</td>
+        </tr>
+    </tbody>
+</table>
--- backend/views/pilgrim/add.php
+++ backend/views/pilgrim/add.php
@@ -38,84 +38,73 @@ $count = 0;
                     </div>
                 <?php ActiveForm::end(); ?>
             </div>
-                <div class="row">
-                    <?php $form = ActiveForm::begin(); ?>
-                    <div class="col-md-10">
+            <div class="row">
+                <?php $form = ActiveForm::begin(); ?>
+                <div class="col-md-10">
 <!--                        --><?php // $form->field($model, 'read')->textarea(['rows' => 4]) ?>
-                        <textarea  name="downloadSourceCode" class="form-control" rows="4"></textarea>
-                    </div>
-                    <div class="col-md-2">
-                        <br>
-                        <br>
-                        <?= Html::submitButton('Saqlash', ['class' => 'btn saqlash btn-primary btn-block']) ?>
-                    </div>
-                    <?php ActiveForm::end(); ?>
+                    <textarea  name="downloadSourceCode" class="form-control" rows="4"></textarea>
                 </div>
-                <div class="row">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                <h3 class="panel-title text-center">Oxirgi kiritilgan ziyoratchi haqida ma'lumot</h3>
-                            </div>
-                            <div class="panel-body">
-                                <div class="col-md-7">
-                                    <ul class="list-group text-success">
-                                        <li class="list-group-item">Familiyasi : <b>surname</b></li>
-                                        <li class="list-group-item">Ismi : <b>name</b></li>
-                                        <li class="list-group-item">Pasport raqami : <b>p_number</b></li>
-                                        <li class="list-group-item">Tug'ilgan kuni : <b>22.12.1997</b></li>
-                                        <li class="list-group-item">Pasport berilgan vaqti : <b>16.02.2019</b></li>
-                                        <li class="list-group-item">Pasport tugash muddati : <b>16.02.2019</b></li>
-                                        <li class="list-group-item">Qachon kiritilganligi : <b>16.02.2019</b></li>
-                                        <li class="list-group-item">Kim kiritganligi : <i>Ismailov D</i></li>
-                                        <li class="list-group-item"><b class="glyphicon glyphicon-triangle-bottom"></b></li>
-                                    </ul>
-                                </div>
-                                <div class="col-md-5">
-                                    <div class="panel panel-info">
-                                    <?php //(file_exists("img/inch/{$findOne['p_number']}.jpg"))?Html::img("/img/inch/{$findOne['p_number']}.jpg",['style'=>'max-width:180px',]):Html::img("/img/dump/nophoto.png",['style'=>'max-width:220px',]);?>
-                                        <input name="img" style="float: left" class="" type="file" id="img">
-                                    </div>
-                                        <div class="row">
-                                            <div class="col-xs-5">
-                                                <select name="mahram_name_id" class="form-control">
-                                                    <option value="<?=null?>">Mahram</option>
-
-                                                </select>
-                                            </div>
-                                            <div class="col-xs-7">
-                                                <select name="mahram_id" class="form-control">
-                                                    <option value="<?=null?>">Mahram kimligi</option>
-
-                                                </select>
-                                            </div>
-                                        </div>
-                                    <select name="function_id" class="form-control">
-
-                                    </select>
-                                    <textarea name="address" class="form-control" placeholder="Yashash joyi haqida ma'lumot" rows="3"></textarea>
-                                    <div class="row">
-                                        <div class="col-xs-7">
-                                            <input name="tel_number" type="text" class="form-control" id="tel" value="" placeholder="Telefon raqami">
-                                        </div>
-                                        <div class="col-xs-5">
-                                            <?= Html::submitButton('Tahrirlash', ['class' => 'btn btn-info btn-block']) ?>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="jumbotron">
-                            <h1 class="text-danger">Guruhda ziyoratchilar<br> yo'q</h1>
-                            <p>Guruhga yana <?=$count_people?> ta ziyoratchi<br> kiritilishi kerak</p>
-                            <!--<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>-->
-                        </div>
-                </div>
-                <br>
-                <br>
-                <div class="alert alert-success jumbotron" role="alert">
-                    <h1 class="text-center text-danger">Guruhda ziyoratchilar to'liq</h1>
+                <div class="col-md-2">
                     <br>
+                    <br>
+                    <?= Html::submitButton('Saqlash', ['class' => 'btn saqlash btn-primary btn-block']) ?>
                 </div>
+                <?php ActiveForm::end(); ?>
+            </div>
+            <div class="row">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3 class="panel-title text-center">Oxirgi kiritilgan ziyoratchi haqida ma'lumot</h3>
+                        </div>
+                        <div class="panel-body">
+                            <div class="col-md-7">
+                                <ul class="list-group text-success">
+                                    <li class="list-group-item">Familiyasi : <b>surname</b></li>
+                                    <li class="list-group-item">Ismi : <b>name</b></li>
+                                    <li class="list-group-item">Pasport raqami : <b>p_number</b></li>
+                                    <li class="list-group-item">Tug'ilgan kuni : <b>22.12.1997</b></li>
+                                    <li class="list-group-item">Pasport berilgan vaqti : <b>16.02.2019</b></li>
+                                    <li class="list-group-item">Pasport tugash muddati : <b>16.02.2019</b></li>
+                                    <li class="list-group-item">Qachon kiritilganligi : <b>16.02.2019</b></li>
+                                    <li class="list-group-item">Kim kiritganligi : <i>Ismailov D</i></li>
+                                    <li class="list-group-item"><b class="glyphicon glyphicon-triangle-bottom"></b></li>
+                                </ul>
+                            </div>
+                            <div class="col-md-5">
+                                <div class="panel panel-info">
+                                <?php //(file_exists("img/inch/{$findOne['p_number']}.jpg"))?Html::img("/img/inch/{$findOne['p_number']}.jpg",['style'=>'max-width:180px',]):Html::img("/img/dump/nophoto.png",['style'=>'max-width:220px',]);?>
+                                    <input name="img" style="float: left" class="" type="file" id="img">
+                                </div>
+                                    <div class="row">
+                                        <div class="col-xs-5">
+                                            <select name="mahram_name_id" class="form-control">
+                                                <option value="<?=null?>">Mahram</option>
+
+                                            </select>
+                                        </div>
+                                        <div class="col-xs-7">
+                                            <select name="mahram_id" class="form-control">
+                                                <option value="<?=null?>">Mahram kimligi</option>
+
+                                            </select>
+                                        </div>
+                                    </div>
+                                <select name="function_id" class="form-control">
+
+                                </select>
+                                <textarea name="address" class="form-control" placeholder="Yashash joyi haqida ma'lumot" rows="3"></textarea>
+                                <div class="row">
+                                    <div class="col-xs-7">
+                                        <input name="tel_number" type="text" class="form-control" id="tel" value="" placeholder="Telefon raqami">
+                                    </div>
+                                    <div class="col-xs-5">
+                                        <?= Html::submitButton('Tahrirlash', ['class' => 'btn btn-info btn-block']) ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            </div>
         </div>
         <div class="col-md-5">
             <ul class="list-group">
@@ -129,39 +118,7 @@ $count = 0;
                 </div>
                 <div class="speed text-success" title="Guruhga yana <?=$count_people-$count?> ta ziyoratchi kiritilishi kerak"><?=$count_people-$count?></div>
             </div>
-            <?php if ($count!=0): ?>
-                <?php $id = 0 ?>
-                <table class="table table-hover table-bordered">
-                    <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Familiya</th>
-                        <th>Ism</th>
-                        <th>Pasport raqam</th>
-                        <th>Tugilgan kun</th>
-                        <th></th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <?php foreach ($panel->all() as $val):?>
-                        <?php $img = (file_exists("img/inch/{$val->p_number}.jpg"))?Html::img("/img/inch/{$val->p_number}.jpg",['class'=>'img-table-add',]):Html::img("/img/dump/nophoto.png",['class'=>'img-table-add-nophoto']);?>
-                        <tr>
-                            <th scope="row"><?=++$id?></th>
-                            <td><?=$val->surname?></td>
-                            <td><?=$val->name?></td>
-                            <td><?=$val->p_number?></td>
-                            <td><?= date('d.m.Y',strtotime($val->d_birth)) ?></td>
-                            <td><?=$img?></td>
-                        </tr>
-                    <?php endforeach;?>
-                    </tbody>
-                </table>
-            <?php else:?>
-                <div class="jumbotron">
-                    <h1 class="text-danger">Guruhda ziyoratchilar yo'q</h1>
-                    <p>Guruhga yana <?=$count_people?> ta ziyoratchi kiritilishi kerak</p>
-                </div>
-            <?php endif;?>
+            <?= $this->render('_right') ?>
         </div>
     </div>
 </div>
