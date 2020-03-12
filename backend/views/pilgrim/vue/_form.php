dc39fee4978e95599604e4bc615c2d2a22100c0b
Doston Ismailov <dostonberuniy@gmail.com>
2020-03-13 02:26:22 +0500

    needMahram

--- backend/views/pilgrim/vue/_form.php
+++ backend/views/pilgrim/vue/_form.php
@@ -47,10 +47,12 @@ use yii\helpers\Url;
             </select>
         </div>
         <div class="col-md-4">
-            <label for="">Mahram</label>
-            <select class="form-control" v-model="pilgrim.mahram_id">
-                <option v-for="(item, key) in mahram_pilgrims" v-bind:value="key">{{ item }}</option>
-            </select>
+            <div v-if="needMahram">
+                <label for="">Mahram</label>
+                <select class="form-control" v-model="pilgrim.mahram_id">
+                    <option v-for="(item, key) in mahram_pilgrims" v-bind:value="key">{{ item }}</option>
+                </select>
+            </div>
         </div>
         <div class="col-md-6">
             <label for="">Form</label>
@@ -68,10 +70,12 @@ use yii\helpers\Url;
             <span class="switchery switchery-default" style="background-color: rgb(19, 218, 254); border-color: rgb(19, 218, 254); box-shadow: rgb(19, 218, 254) 0px 0px 0px 16px inset; transition: border 0.4s ease 0s, box-shadow 0.4s ease 0s, background-color 1.2s ease 0s;"><small style="left: 20px; transition: background-color 0.4s ease 0s, left 0.2s ease 0s; background-color: rgb(255, 255, 255);"></small></span>
         </div>
         <div class="col-md-4">
-            <label for="">Mahram name</label>
-            <select class="form-control" v-model="pilgrim.mahram_name_id">
-                <option v-for="mahram_name in mahram_names" v-bind:value="mahram_name.id">{{ mahram_name.name }}</option>
-            </select>
+            <div v-if="needMahram">
+                <label for="">Mahram name</label>
+                <select class="form-control" v-model="pilgrim.mahram_name_id">
+                    <option v-for="mahram_name in mahram_names" v-bind:value="mahram_name.id">{{ mahram_name.name }}</option>
+                </select>
+            </div>
         </div>
     </div>
 </div>
@@ -95,6 +99,7 @@ use yii\helpers\Url;
                 flights: <?= Json::encode($flights)?>,
                 flight_id: '',
                 validateErrMsg: '',
+                needMahram: false,
                 pilgrim: { // 20 ta
                     region_id: '',
                     group_id: '',
@@ -125,21 +130,29 @@ use yii\helpers\Url;
                 let document = new MRZ.Document(this.pilgrim.p_mrz).parse();
 
                 let parents = this.$parent.pilgrims;
+                let allowNoMahram = true;
                 if (this.validate(document)){
-                    axios.post('<?= Url::to(['create']) ?>', {
-                        Pilgrim: this.pilgrim
-                    })
-                        .then(function (response) {
-                            if (response.data.success === true){
-                                parents.push(response.data.model);
-                                alert('Added successfully!');
-                            } else {
-                                alert('Error');
-                            }
+                    if (this.needMahram){
+                        if (!confirm("Mahramsiz ham kiritaverasizmi?")){
+                            allowNoMahram = false;
+                        }
+                    }
+                    if (allowNoMahram){
+                        axios.post('<?= Url::to(['create']) ?>', {
+                            Pilgrim: this.pilgrim
                         })
-                        .catch(function (error) {
-                            console.log(error);
-                        });
+                            .then(function (response) {
+                                if (response.data.success === true){
+                                    parents.push(response.data.model);
+                                    // this.clearFields();
+                                } else {
+                                    alert('Error');
+                                }
+                            })
+                            .catch(function (error) {
+                                console.log(error);
+                            });
+                    }
                 } else {
                     console.log(this.validateErrMsg);
                     alert('not sent')
@@ -229,13 +242,16 @@ use yii\helpers\Url;
                     errMsg = errMsg.concat('p_mrz ni tanlang\n');
                     valid = false;
                 }
-                if (!this.pilgrim.mahram_name_id){
-                    errMsg = errMsg.concat('Mahram name ni tanlang\n');
-                    valid = false;
-                }
-                if (!this.pilgrim.mahram_id){
-                    errMsg = errMsg.concat('mahram_id ni tanlang\n');
-                    valid = false;
+                if (this.calculateAge(this.pilgrim.birth_date) < 45 && this.pilgrim.gender == 'F'){
+                    this.needMahram = true;
+                    if (!this.pilgrim.mahram_name_id){
+                        errMsg = errMsg.concat('Mahram name ni tanlang\n');
+                        // valid = false;
+                    }
+                    if (!this.pilgrim.mahram_id){
+                        errMsg = errMsg.concat('mahram_id ni tanlang\n');
+                        // valid = false;
+                    }
                 }
                 if (!this.get_nationality_id(document.nationality)){
                     errMsg = document.nationality + ' country doesn\'t found\n';
@@ -247,10 +263,14 @@ use yii\helpers\Url;
             },
             get_issue_date: function(document){
                 let expiry_date = new Date(document.document_expiry);
-                let year = expiry_date.getFullYear();
-                let month = expiry_date.getMonth();
-                let day = expiry_date.getDate();
-                let p_issue_date = new Date(year - 10, month, day + 1);
+                let ex_year = expiry_date.getFullYear();
+                let ex_month = expiry_date.getMonth();
+                let ex_day = expiry_date.getDate();
+
+                let p_issue_date = new Date(ex_year - 10, ex_month, ex_day + 1);
+                let year = p_issue_date.getFullYear();
+                let month = p_issue_date.getMonth();
+                let day = p_issue_date.getDate();
                 if (document.document_issue){
                     return document.document_issue;
                 } else {
@@ -275,6 +295,33 @@ use yii\helpers\Url;
             },
             getGroups: function(flight_id) {
                 axios.get("<?= Url::to(['load-groups']) ?>", { params:{flight_id: flight_id} }).then(response => this.groups = response.data);
+            },
+            calculateAge: function(birthday) { // birthday is a date
+                let ageDifMs = Date.now() - new Date(birthday).getTime();
+                let ageDate = new Date(ageDifMs); // milliseconds from epoch
+                return Math.abs(ageDate.getUTCFullYear() - 1970);
+            },
+            clearFields: function () {
+                this.pilgrim.region_id = '';
+                this.pilgrim.group_id = '';
+                this.pilgrim.pilgrim_type_id = 1;
+                this.pilgrim.mahram_name_id = '';
+                this.pilgrim.mahram_id = null;
+                this.pilgrim.marital_status = 1;
+                this.pilgrim.p_mrz = 'doston';
+                this.pilgrim.first_name = '';
+                this.pilgrim.last_name = '';
+                this.pilgrim.middle_name = '';
+                this.pilgrim.birth_date = '';
+                this.pilgrim.gender = '';
+                this.pilgrim.p_number = '';
+                this.pilgrim.p_issue_date = '';
+                this.pilgrim.p_expiry_date = '';
+                this.pilgrim.p_type = '';
+                this.pilgrim.nationality_id = '';
+                this.pilgrim.personal_number = '';
+                this.pilgrim.status = 1;
+                this.pilgrim.user_id = <?= Yii::$app->user->id?>;
             }
         },
         mounted(){
